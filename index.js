4f073ba13e13c1645d300d10d1ce98f581005fe1
kimJiHwan0618 <wlghks0618@kakao.com>
2023-05-16 01:32:03 +0900

    cors test

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 
-// const corsOptions = {
-//   origin: [
-//     'http://caribo.me',
-//     'https://caribo.me',
-//     'http://admin.mypopol.com',
-//     'https://admin.mypopol.com',
-//     'https://kimjihodo.synology.me',
-//     'http://localhost:3001',
-//     'http://127.0.0.1:5500',
-//   ],
-// };
+const corsOptions = {
+  origin: [
+    'http://caribo.me',
+    'https://caribo.me',
+    'http://admin.mypopol.com',
+    'https://admin.mypopol.com',
+    'https://kimjihodo.synology.me',
+    'http://localhost:3001',
+    'http://127.0.0.1:5500',
+  ],
+};
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
+// app.use(cors());
 
 const port = 3000;
 app.listen(port, () => {
@@ -29,4 +29,3 @@ app.listen(port, () => {
 // api ctrl
 app.use('/email', require('./routes/emailRouter'));
 app.use('/auth', require('./routes/authRouter'));
-// app.use('/db', require('./routes/dbRouter'));
