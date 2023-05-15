958e4a0963eb0269f6195217ff4629657671cd16
kimJiHwan0618 <wlghks0618@kakao.com>
2023-05-15 22:06:15 +0900

    cors test

--- index.js
+++ index.js
@@ -3,21 +3,23 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 
-const corsOptions = {
-  origin: [
-    'http://caribo.me',
-    'https://caribo.me',
-    'http://admin.mypopol.com',
-    'https://admin.mypopol.com',
-    'http://localhost:3001',
-    'http://127.0.0.1:5500',
-  ],
-};
+// const corsOptions = {
+//   origin: [
+//     'http://caribo.me',
+//     'https://caribo.me',
+//     'http://admin.mypopol.com',
+//     'https://admin.mypopol.com',
+//     'https://kimjihodo.synology.me',
+//     'http://localhost:3001',
+//     'http://127.0.0.1:5500',
+//   ],
+// };
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
+// app.use(cors(corsOptions));
 
 const port = 3000;
 app.listen(port, () => {
