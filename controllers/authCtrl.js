db42a1486ed73e313b5c600154b59e669fcc9579
kimJiHwan0618 <wlghks0618@kakao.com>
2023-05-19 23:52:04 +0900

    로그인 서버 요청 에러시 로그 확인

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -6,44 +6,42 @@ const dbCtrl = {
   signIn: async (req, res) => {
     const clientDomain = req.headers.origin;
     console.log('Client domain:', clientDomain);
-    connection.query(query.getUser(req.body),
-      (error, rows) => {
-        // if (error) throw error;
-        try {
-          if (rows.length === 1) {
-            const user = {
-              key: `${rows[0].userKey}`,
-              userId: `${rows[0].userId}`,
-              username: `${rows[0].userName}`,
-              roleId: `${rows[0].roleId}`,
-              role: `${rows[0].roleName}`,
-            };
-            const token = jwt.sign(user, 'my_secret_key', { expiresIn: '30m' });
-            res.cookie('token', token, { httpOnly: true });
-            res.send({
-              code: 200,
-              response: {
-                ...{
-                  accessToken: token,
-                  tokenExpiresIn: new Date().getTime(),
-                },
-                ...user,
+    connection.query(query.getUser(req.body), (error, rows) => {
+      // if (error) throw error;
+      try {
+        if (rows.length === 1) {
+          const user = {
+            key: `${rows[0].userKey}`,
+            userId: `${rows[0].userId}`,
+            username: `${rows[0].userName}`,
+            roleId: `${rows[0].roleId}`,
+            role: `${rows[0].roleName}`,
+          };
+          const token = jwt.sign(user, 'my_secret_key', { expiresIn: '5m' });
+          res.cookie('token', token, { httpOnly: true });
+          res.send({
+            code: 200,
+            response: {
+              ...{
+                accessToken: token,
+                tokenExpiresIn: new Date().getTime(),
               },
-            });
-          } else {
-            const code = 401;
-            res.status(code).json({
-              code: code,
-              status: 'Unauthorized',
-              message: '일치하는 유저 정보가 없습니다.',
-              timestamp: new Date(),
-            });
-          }
-        } catch (err) {
-          return res.status(401).json({ error: '로그인 요청 에러' });
+              ...user,
+            },
+          });
+        } else {
+          const code = 401;
+          res.status(code).json({
+            code: code,
+            status: 'Unauthorized',
+            message: '일치하는 유저 정보가 없습니다.',
+            timestamp: new Date(),
+          });
         }
+      } catch (err) {
+        return res.status(401).json({ error });
       }
-    );
+    });
   },
   // signIn
   accessToken: async (req, res) => {
@@ -52,7 +50,7 @@ const dbCtrl = {
       const decodedToken = jwt.verify(token, 'my_secret_key');
       delete decodedToken.iat;
       delete decodedToken.exp;
-      const refreshToken = jwt.sign(decodedToken, 'my_secret_key', { expiresIn: '30m' });
+      const refreshToken = jwt.sign(decodedToken, 'my_secret_key', { expiresIn: '5m' });
       res.cookie('token', refreshToken, { httpOnly: true });
       res.send({
         code: 200,
