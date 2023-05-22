5cddbddbf1c520ee75050c6bc0b7571c23a50895
KimAJksldq <wlghks0106@naver.com>
2023-05-22 17:41:53 +0900

    sftp 접속, 디렉토리 조회 예제코드 추가

--- routes/commonRouter.js
+++ routes/commonRouter.js
@@ -2,5 +2,6 @@ const commonCtrl = require('../controllers/commonCtrl');
 const router = require('express').Router();
 
 router.route('/sideMenus').get(commonCtrl.getMenus);
+router.route('/sftpTest').post(commonCtrl.testSftp);
 
 module.exports = router;
