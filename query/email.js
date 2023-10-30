db2626b0f87a2dad0076583aab26ed717fa1953c
kimJiHwan0618 <wlghks0618@kakao.com>
2023-10-31 00:05:07 +0900

    쿼리 컬럼 수정

--- query/email.js
+++ query/email.js
@@ -2,7 +2,7 @@ const email = {
   insertMailCount: (param) => {
     return `
       INSERT INTO mail_sent_count (userId, ptId, email, title, phone, subject, userIp, cdate) 
-      VALUES ('${param.userId}', '${param.ptId}', '${param.from}', '${param.title}', '${param.phone}', '${param.phone}', '${param.userIp}' , NOW());
+      VALUES ('${param.userId}', '${param.ptId}', '${param.from}', '${param.title}', '${param.phone}', '${param.subject}', '${param.userIp}' , NOW());
   `;
   },
 };
