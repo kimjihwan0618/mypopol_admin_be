b36d9e9ece0a3188468243cc090a9811ea56a2fa
kimJiHwan0618 <78426129+kimJiHwan0618@users.noreply.github.com>
2024-01-24 22:31:59 +0900

    소스머지

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,9 +1,9 @@
 const authCtrl = require('../controllers/authCtrl');
 const router = require('express').Router();
 
-router.route('/login').post(authCtrl.signIn);
-router.route('/access-token').get(authCtrl.accessToken);
-router.route('/post/signup-code').post(authCtrl.signCodePub);
+router.route('/login').post(authCtrl.postSignIn);
+router.route('/access-token').get(authCtrl.postAccessToken);
+router.route('/post/signup-code').post(authCtrl.postAuthCode);
 router.route('/get/user').get(authCtrl.getUser);
 
 module.exports = router;
