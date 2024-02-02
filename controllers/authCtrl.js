899befe3468cb8661338e54110997fbc098b761d
kimJiHwan0618 <78426129+kimJiHwan0618@users.noreply.github.com>
2024-02-03 00:31:27 +0900

    postUser 작업중.. index.html 파일 완전카피 개선해야됨

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -1,6 +1,9 @@
 const root = require.main.path;
 const path = require('path');
 const log4js = require('log4js');
+const Client = require('ssh2-sftp-client');
+const sftp = new Client();
+const sftpConfig = require(path.join(root, 'config/sftp.config'));
 const dbPool = require(path.join(root, 'config/db.config'));
 const nodemailer = require('nodemailer');
 const query = require(path.join(root, 'query/auth'));
@@ -152,10 +155,30 @@ const dbCtrl = {
   postUser: async (req, res) => {
     const connection = await dbPool.getConnection();
     try {
+      const { templateId, userId } = req.body;
       await connection.beginTransaction();
       await connection.query(query.postUser(req.body));
       await connection.query(query.postPopol(req.body));
-      await connection.commit()
+      await sftp.connect(sftpConfig);
+      const template_def_src = `/web/site/${templateId}`;
+      await sftp.mkdir(`${template_def_src}/${userId}`, true);
+      const fileList = await sftp.list(`${template_def_src}/example`);
+      for (const file of fileList) {
+        if (file.type !== 'd') { // 파일 타입 폴더 아닐시만 복사
+          const sourcePath = `${template_def_src}/example/${file.name}`
+          const destinationPath = `${template_def_src}/${userId}/${file.name}`
+          if (path.extname(file.name) === '.js') {
+            await userInfoFileWrite(sourcePath, destinaztionPath, userId);
+            console.log(`${file.name} 파일이 수정되었습니다.`);
+          } else {
+            const fileContent = await sftp.fastGet(sourcePath, "utf-8");
+            await sftp.put(Buffer.from(fileContent), destinationPath); //파일 복사 
+
+          }
+          console.log(`${file.name} 파일이 복사되었습니다.`);
+        }
+      }
+
       res.status(200).send({
         // 유저발급키 생성후 바로 로그인 넘어갈지 검토
       });
@@ -170,6 +193,21 @@ const dbCtrl = {
       connection.release();
     }
   },
+  userInfoFileWrite: async (sourcePath, destinationPath, userId) => {
+    try {
+      // 원격 서버의 파일 읽기
+      const fileContent = await sftp.get(sourcePath, null);
+
+      // 파일 내용 수정
+      const modifiedContent = fileContent.replace("const userId = 'example'", `const userId = '${userId}'`);
+
+      // 수정된 내용을 원격 서버의 파일에 쓰기
+      await sftp.put(Buffer.from(modifiedContent), destinationPath);
+    } catch (error) {
+      console.error('파일 수정 및 복사 중 에러 발생:', error);
+    }
+  },
+
 };
 
 module.exports = dbCtrl;
