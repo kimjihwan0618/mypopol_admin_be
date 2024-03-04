d0206be604c5b91dbc3dc5e0af2ec839322c69d4
kimJiHwan0618 <78426129+kimJiHwan0618@users.noreply.github.com>
2024-03-04 11:56:24 +0900

    insertMailCount DB 컬럼 수정

--- query/email.js
+++ query/email.js
@@ -1,7 +1,7 @@
 const email = {
   insertMailCount: (param) => {
     return `
-      INSERT INTO mail_sent_count (userId, ptId, email, title, phone, subject, userIp, cdate) 
+      INSERT INTO mail_sent_count (userId, ptId, email, title, phone, subject, userIp, timeStamp) 
       VALUES ('${param.userId}', '${param.ptId}', '${param.from}', '${param.title}', '${param.phone}', '${param.subject}', '${param.userIp}' , NOW());
   `;
   },
