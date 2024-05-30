319db1f7abfddc2f6e78b1dd6b089dd558656406
kimJiHwan0618 <78426129+kimJiHwan0618@users.noreply.github.com>
2024-05-30 22:33:58 +0900

    email/send api 스웨거 추가

--- controllers/emailCtrl.js
+++ controllers/emailCtrl.js
@@ -17,40 +17,44 @@ const emailCtrl = {
     try {
       req.body.userIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
       const { from, to, subject, html, pw, title } = req.body;
-      if (pw === 'WlGhks010!@#') {
-        const transporter = nodemailer.createTransport({
-          host: emailAuth.host,
-          port: 465,
-          secure: true,
-          auth: {
-            user: emailAuth.user,
-            pass: emailAuth.pass,
-          },
-        });
-        const mailOptions = {
-          from: emailAuth.user,
-          to: to,
-          subject: `[${subject}] ${title}`,
-          html: html,
-        };
-        const info = await transporter.sendMail(mailOptions);
-        const [insertResult, error2] = await connection.query(query.insertMailCount(req.body))
-        const [rows] = await connection.query("SELECT * FROM mail_sent_count WHERE seq = ?", [insertResult.insertId]);
-        const ws = clientSessions.get(req.body.userId);
-        if (ws && ws.readyState === WebSocket.OPEN) {
-          logger.info(`WS - 이메일 카운트 [Info] : ${req.body.userId}`);
-          ws.send(JSON.stringify({
+      const transporter = nodemailer.createTransport({
+        host: emailAuth.host,
+        port: 465,
+        secure: true,
+        auth: {
+          user: emailAuth.user,
+          pass: emailAuth.pass,
+        },
+      });
+      const mailOptions = {
+        from: emailAuth.user,
+        to: to,
+        subject: `[${subject}] ${title}`,
+        html: html,
+      };
+      const info = await transporter.sendMail(mailOptions);
+      const [insertResult, error2] = await connection.query(query.insertMailCount(req.body));
+      const [rows] = await connection.query('SELECT * FROM mail_sent_count WHERE seq = ?', [
+        insertResult.insertId,
+      ]);
+      const ws = clientSessions.get(req.body.userId);
+      if (ws && ws.readyState === WebSocket.OPEN) {
+        logger.info(`WS - 이메일 카운트 [Info] : ${req.body.userId}`);
+        ws.send(
+          JSON.stringify({
             type: '이메일 카운트',
             ptId: req.body.ptId,
             userId: req.body.userId,
-            data: rows[0]
-          }));
-        }
-        logger.info(`Mail Send -> From : ${from}, To : ${to}`);
-        res.status(200).json({ success: true, message: 'Mail sent successfully.' });
-      } else {
-        res.status(403).json({ success: false, message: 'Invalid password.' });
+            data: rows[0],
+          })
+        );
       }
+      logger.info(`Mail Send -> From : ${from}, To : ${to}`);
+      res.status(200).json({ success: true, message: 'Mail sent successfully.' });
+      // }
+      // else {
+      //   res.status(403).json({ success: false, message: 'Invalid password.' });
+      // }
     } catch (err) {
       res.status(500).json({
         message: 'sendMail error : 내부 서버 오류가 발생했습니다.',
---
+++ swagger/site/email.js
@@ -0,0 +1,62 @@
+/**
+ * @swagger
+ * /email/send:
+ *   post:
+ *     summary: 사이트 CONTACT 이메일 전송 API
+ *     tags : [User Public]
+ *     requestBody:
+ *       description: 이메일 전송에 쓰는 속성값
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               phone:
+ *                 type: string
+ *                 description: 발신자 휴대폰 번호(-제외)
+ *               userId:
+ *                 type: string
+ *                 description: 수신자 유저 ID
+ *               ptid:
+ *                 type: string
+ *                 description: 사이트 템플릿 ID (ptid01, ptid02)
+ *               ptid:
+ *                 type: string
+ *                 description: 사이트 템플릿 ID (ptid01, ptid02)
+ *               from:
+ *                 type: string
+ *                 description: 발신자 이메일
+ *               to:
+ *                 type: string
+ *                 description: 수신자 이메일
+ *               subject:
+ *                 type: string
+ *                 description: 이메일 내용 유형
+ *               title:
+ *                 type: string
+ *                 description: 이메일 제목
+ *               content:
+ *                 type: string
+ *                 description: 이메일 내용(body)
+ *               html:
+ *                 type: string
+ *                 description: 실제 이메일이 보여질 html 입력
+ *     responses:
+ *       200:
+ *         description: 이메일 전송 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *                 type: object
+ *                 properties:
+ *                   popolInfo:
+ *                     type: object
+ *                     properties:
+ *                       success:
+ *                         type: boolean
+ *                       message:
+ *                         type: string
+ *       500:
+ *         description: 서버 에러
+ */
